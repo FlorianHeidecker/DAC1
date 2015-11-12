b00814119a1932c6ad6ed6da4d6197c640944c1a
Sebastian-Wolfarth <sebastian.wolfarth@gmx.net>
2015-11-12 22:07:09 +0100

    First Implementation of XLCD
    There is still a problem with the defines. not sure if this is useable
    with our device

--- source/main.c
+++ source/main.c
@@ -8,20 +8,14 @@
 #include <xc.h>
 #include <stdio.h>
 
-#include "menu.h"
-#include "data.h"
 #include "log.h"
+#include "xlcd.h"
 
 
 int main(void) {
     LOG("main()\n");
     
     
-    printf("\n==test data============================================\n");
-    printf("%s%s\n",get_name_text(0,1), get_value_text(0,1));
-    set_setting(0,1,2); // change setting to off
-    printf("%s%s\n",get_name_text(0,1), get_value_text(0,1));
-    printf("\n==test data============================================\n");
     
     
     while(1);
