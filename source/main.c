c5fee4613d29ceb3444c861044cfdd3f76c3bc18
FlorianHeidecker <flo.92@online.de>
2015-11-13 15:44:48 +0100

    Cleanup
    data.c/h and menu.c/h deleted
    main also cleaned

--- source/main.c
+++ source/main.c
@@ -8,21 +8,12 @@
 #include <xc.h>
 #include <stdio.h>
 
-#include "menu.h"
-#include "data.h"
 #include "log.h"
 
 
 int main(void) {
     LOG("main()\n");
     
-    
-    printf("\n==test data============================================\n");
-    printf("%s%s\n",get_name_text(0,1), get_value_text(0,1));
-    set_setting(0,1,2); // change setting to off
-    printf("%s%s\n",get_name_text(0,1), get_value_text(0,1));
-    printf("\n==test data============================================\n");
-    
-    
-    while(1);
+    LOG("main() end\n");
+    return 0;
 }
