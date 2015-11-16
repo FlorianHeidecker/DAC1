7f51b0de1cda36f7748a4acc4bbc0c87b6554222
FlorianHeidecker <flo.92@online.de>
2015-11-16 11:40:10 +0100

    Update PLL_API.c

--- source/PLL_API.c
+++ source/PLL_API.c
@@ -26,50 +26,50 @@ void pll_init(){
     PLL_FS1_TRIS   = 0; /* output */
     PLL_FS2_TRIS   = 0; /* output */
     
-    set_SCKO1(_33);     /* default value */
-    set_SCKO23(_44_1);  /* default value */
+    set_SCKO1(PLL_SCKO1_33MHz);     /* default value */
+    set_SCKO23(PLL_SAMPLING_FREQ_44_1kHz);  /* default value */
 }
 
 void set_SCKO1(SCKO1_FREQ scko1_freq){
-    if(scko1_freq == _16) LOG("set SCKO1 FREQUENCY = _16\n");
-    if(scko1_freq == _33) LOG("set SCKO1 FREQUENCY = _33\n");
+    if(scko1_freq == PLL_SCKO1_16MHz) LOG("set SCKO1 FREQUENCY = PLL_SCKO1_16MHz\n");
+    if(scko1_freq == PLL_SCKO1_33MHz) LOG("set SCKO1 FREQUENCY = PLL_SCKO1_33MHz\n");
     PLL_CSEL_LATCH = scko1_freq;
 }
 
 void set_SCKO23(SAMPLING_FREQ sampling_freq){
     switch(sampling_freq){
-        case _32:
-            LOG("set SAMPLING FREQUENCY = _32\n");
+        case PLL_SAMPLING_FREQ_32kHz:
+            LOG("set SAMPLING FREQUENCY = PLL_SAMPLING_FREQ_32kHz\n");
             PLL_SR_LATCH  = 0;
             PLL_FS1_LATCH = 0;
             PLL_FS2_LATCH = 1;
             break;
-        case _44_1:
-            LOG("set SAMPLING FREQUENCY = _44_1\n");
+        case PLL_SAMPLING_FREQ_44_1kHz:
+            LOG("set SAMPLING FREQUENCY = PLL_SAMPLING_FREQ_44_1kHz\n");
             PLL_SR_LATCH  = 0;
             PLL_FS1_LATCH = 1;
             PLL_FS2_LATCH = 0;
             break;
-        case _48:
-            LOG("set SAMPLING FREQUENCY = _48\n");
+        case PLL_SAMPLING_FREQ_48kHz:
+            LOG("set SAMPLING FREQUENCY = PLL_SAMPLING_FREQ_48kHz\n");
             PLL_SR_LATCH  = 0;
             PLL_FS1_LATCH = 0;
             PLL_FS2_LATCH = 0;
             break;
-        case _64:
-            LOG("set SAMPLING FREQUENCY = _64\n");
+        case PLL_SAMPLING_FREQ_64kHz:
+            LOG("set SAMPLING FREQUENCY = PLL_SAMPLING_FREQ_64kHz\n");
             PLL_SR_LATCH  = 1;
             PLL_FS1_LATCH = 0;
             PLL_FS2_LATCH = 1;
             break;
-        case _88_2:
-            LOG("set SAMPLING FREQUENCY = _88_2\n");
+        case PLL_SAMPLING_FREQ_88_2kHz:
+            LOG("set SAMPLING FREQUENCY = PLL_SAMPLING_FREQ_88_2kHz\n");
             PLL_SR_LATCH  = 1;
             PLL_FS1_LATCH = 1;
             PLL_FS2_LATCH = 0;
             break;
-        case _96:
-            LOG("set SAMPLING FREQUENCY = _96\n");
+        case PLL_SAMPLING_FREQ_96kHz:
+            LOG("set SAMPLING FREQUENCY = PLL_SAMPLING_FREQ_96kHz\n");
             PLL_SR_LATCH  = 1;
             PLL_FS1_LATCH = 0;
             PLL_FS2_LATCH = 0;
