e280259976934ec3c0eb144004b2cb8b967203c4
Sebastian-Wolfarth <sebastian.wolfarth@gmx.net>
2015-11-23 20:44:09 +0100

    update

--- source/main.c
+++ source/main.c
@@ -5,6 +5,9 @@
  * Created on 29. Oktober 2015, 15:31
  */
 
+ #define FOSC    (7370000UL)
+ #define FCY     (FOSC/2)
+
 #include <xc.h>
 #include <stdio.h>
 
@@ -17,6 +20,18 @@ int main(void) {
     LOG("main()\n");
     
     OpenXLCD(FOUR_BIT & LINES_5X7);
+    
+    while(BusyXLCD() );
+    WriteCmdXLCD(SHIFT_CUR_RIGHT);
+
+    while(BusyXLCD() );
+    WriteCmdXLCD(DON & CURSOR_OFF & BLINK_OFF); 
+	
+    while(BusyXLCD() );
+    WriteCmdXLCD(0b00000010);   // line 1 pos 1
+
+    while( BusyXLCD() );
+    putrsXLCD("Welcome");
     
     pll_init();
     
--- source/xlcd/delays.c
+++ source/xlcd/delays.c
@@ -5,10 +5,15 @@
  * Created on 23. November 2015, 20:10
  */
 
-#define FCY     (700000/2)
+ #define FOSC    (7370000UL)
+ #define FCY     (FOSC/2)
 
 #include <xc.h>
 #include "delays.h"
+
+/* definition of typical duration of loop containg clear watchdog */
+/* given in TCYs */
+#define TCY_CNT_PER_LOOP 4
 
 
 void DelayFor18TCY (void)
@@ -17,18 +22,35 @@ void DelayFor18TCY (void)
 
    for( k=0; k<18; k++)
    {
-       asm("nop\n\t");
+       asm("nop");
       //__nop();
    }
 }
-void DelayPORXLCD (void)
+//void DelayPORXLCD (void)
+//{
+//   __delay_ms(15);
+//}
+
+void DelayPORXLCD(void) 	/* provides delay for atleast 15msec */
 {
-   //__delay_ms(15);
+    /* Give a delay of atleast 15 msec after power up	*/
+    /* POR Power On Reset */
+    
+   long i;
+    for(i=0;i < DelayPORXLCD_Cnt;i+=TCY_CNT_PER_LOOP)
+        asm("clrwdt");
 }
 
 void DelayXLCD (void)
 {
    //__delay_ms(5);
+}
+
+void DelayXLCD(void)  		/* provides delay for atleast 5msec */
+{
+  long i;
+    for(i=0;i < DelayXLCD_Cnt;i+=TCY_CNT_PER_LOOP)
+        asm("clrwdt");
 }
 
 
