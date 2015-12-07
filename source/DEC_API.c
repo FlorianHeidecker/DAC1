47f9fecbd09fe29e442e2acd2cbdd64c9b38046a
PhilippSchenk <s6978@h-ab.de>
2015-12-07 16:49:33 +0100

    update
    new detection works with edges...
    tested and verified with oszi and UART output

--- source/DEC_API.c
+++ source/DEC_API.c
@@ -10,7 +10,7 @@
  * @author      Philipp Schenk
  * @date        18.11.2015 - initial version
  * 
- * @version     0.3
+ * @version     0.4
  *
  */
 
@@ -21,46 +21,63 @@
 #include "DEC_API.h"
 
 
-static uint16_t dec_last = 0x00;
+static state_rotation_t button_old = DEC_NO_TURN;
+static state_rotation_t dec_old = DEC_NO_TURN;
 
 void DEC_init(void){
     AD1PCFGL       |= 0x1800; /*Set AN11, AN12 to Digital Pins*/ 
     DEC_CHA_TRIS    = 1; /* input */
     DEC_CHB_TRIS    = 1; /* input */
     DEC_TASTE_TRIS  = 1; /* input */
-    dec_last        = (!DEC_CHB_PORT <<1) | (!DEC_CHA_PORT);
 }
 
 unsigned char get_DEC_button(void){
-    return (!DEC_TASTE_PORT);
+    if(!DEC_TASTE_PORT && button_old == DEC_NO_TURN){
+        button_old = DEC_BUTTON;
+        return DEC_BUTTON;
+    }
+    else if(DEC_TASTE_PORT && button_old == DEC_BUTTON){
+        button_old = DEC_NO_TURN;
+        return DEC_NO_TURN;
+    }
+    else
+        return DEC_NO_TURN;
 }
 
 state_rotation_t get_DEC_status(void){
     uint16_t dec_inp = 0x00;
     
-    if(!DEC_TASTE_PORT){
+    if(!DEC_TASTE_PORT && button_old == DEC_NO_TURN){
+        button_old = DEC_BUTTON;
         return DEC_BUTTON;
     }
+    else if(DEC_TASTE_PORT && button_old == DEC_BUTTON){
+        button_old = DEC_NO_TURN;
+        return DEC_NO_TURN;
+    }
     else{
-        dec_inp = (!DEC_CHB_PORT <<1) | (!DEC_CHA_PORT);
-        
-        if(dec_inp == dec_last){
+        dec_inp = (DEC_CHB_PORT <<4) | (DEC_CHA_PORT);
+        if(dec_inp ==0x10 && dec_old == DEC_NO_TURN){
+            dec_old = DEC_TURN_RIGHT;
             return DEC_NO_TURN;
         }
-        
-        dec_inp  = dec_inp + dec_last;
-        dec_last = dec_inp - dec_last;
-        
-        if((dec_inp == 0x02) || (dec_inp == 0x04) ){
-            return DEC_TURN_LEFT;
-        }
-        else if((dec_inp == 0x01) || (dec_inp == 0x05) ){
+        else if(dec_inp ==0x11 && dec_old == DEC_TURN_RIGHT){
+            dec_old = DEC_NO_TURN;
             return DEC_TURN_RIGHT;
+        }
+        else if(dec_inp == 0x00 && dec_old == DEC_NO_TURN){
+            dec_old = DEC_TURN_LEFT;
+            return DEC_NO_TURN;
+        }
+        else if(dec_inp ==0x11 && dec_old == DEC_TURN_LEFT){
+            dec_old = DEC_NO_TURN;
+            return DEC_TURN_LEFT;
         }
         else{
             return DEC_NO_TURN;
         }
-    }  
+            
+    }
 }
 
 
