ba8b6557ffadf08a40787feb024a677a62c2db20
FlorianHeidecker <flo.92@online.de>
2015-12-07 18:13:37 +0100

    first implementation

---
+++ source/CONTROL.c
@@ -0,0 +1,21 @@
+/** 
+ * @file        CONTROL.c
+ *
+ * @brief       CONTROL controls all input and output set and get function
+ * 
+ * @author      Florian Heidecker
+ * @date        07.12.2015 - initial version
+ * 
+ * @version     0.1
+ */
+
+#include "xc.h"
+
+#include "log.h"
+#include "global.h"
+#include "CONTROL.h"
+#include "PLL_API.h"
+
+void CONTROL_init(void){
+    
+}
---
+++ source/CONTROL.h
@@ -0,0 +1,20 @@
+/** 
+ * @file        CONTROL.h
+ *
+ * @brief       CONTROL controls all input and output set and get function
+ * 
+ * @author      Florian Heidecker
+ * @date        07.12.2015 - initial version
+ * 
+ * @version     0.1
+ */
+
+#ifndef _CONTROL_H
+#define	_CONTROL_H
+
+#include <xc.h>
+
+void CONTROL_init(void);
+
+#endif	/* _CONTROL_H */
+
