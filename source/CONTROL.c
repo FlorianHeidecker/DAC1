9d0a3d3a1bf31721c153ae2020ada78050b7b153
FlorianHeidecker <flo.92@online.de>
2015-12-08 09:03:31 +0100

    update

--- source/CONTROL.c
+++ source/CONTROL.c
@@ -19,3 +19,39 @@
 void CONTROL_init(void){
     
 }
+
+void CONTROL_set_attunation_level(uint8_t value){
+    
+}
+
+uint8_t CONTROL_get_attunation_level(void){
+    
+}
+
+void CONTROL_set_soft_mute(uint8_t value){
+    
+}
+
+uint8_t CONTROL_get_soft_mute(void){
+    
+}
+
+void CONTROL_set_zero_detect_mute(uint8_t value){
+    
+}
+
+uint8_t CONTROL_get_zero_detect_mute(void){
+    
+}
+
+void CONTROL_set_monaural_mode(PCM_monaural_mode_t PCM_monaural_mode){
+    
+}
+
+PCM_monaural_mode_t CONTROL_get_monaural_mode(void){
+    
+}
+
+uint8_t CONTROL_get_zero_detection(void){
+    
+}
