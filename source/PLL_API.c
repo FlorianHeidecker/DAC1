604d5a59cb00335c4b67731b8529262f080880a3
FlorianHeidecker <flo.92@online.de>
2015-12-10 12:00:37 +0100

    name fix

--- source/PLL_API.c
+++ source/PLL_API.c
@@ -16,17 +16,17 @@
 #include "global.h"
 #include "PLL_API.h"
 
-void set_scko1_freq(PLL_scko1_freq_t PLL_scko1_freq){
+void PLL_set_scko1_freq(PLL_scko1_freq_t PLL_scko1_freq){
     if(PLL_scko1_freq == PLL_SCKO1_16MHz) PLL_LOG("PLL_LOG: set SCKO1 FREQUENCY = PLL_SCKO1_16MHz\n");
     if(PLL_scko1_freq == PLL_SCKO1_33MHz) PLL_LOG("PLL_LOG: set SCKO1 FREQUENCY = PLL_SCKO1_33MHz\n");
     PLL_CSEL_LATCH = PLL_scko1_freq;
 }
 
-PLL_scko1_freq_t get_scko1_freq(void){
+PLL_scko1_freq_t PLL_get_scko1_freq(void){
     return PLL_CSEL_LATCH;
 }
 
-void set_sampling_freq(PLL_sampling_freq_t PLL_sampling_freq){
+void PLL_set_sampling_freq(PLL_sampling_freq_t PLL_sampling_freq){
     switch(PLL_sampling_freq){
         case PLL_SAMPLING_FREQ_32kHz:
             PLL_LOG("PLL_LOG: set SAMPLING FREQUENCY = PLL_SAMPLING_FREQ_32kHz\n");
@@ -67,7 +67,7 @@ void set_sampling_freq(PLL_sampling_freq_t PLL_sampling_freq){
     }
 }
 
-PLL_sampling_freq_t get_sampling_freq(void){
+PLL_sampling_freq_t PLL_get_sampling_freq(void){
     PLL_sampling_freq_t index = PLL_SR_LATCH*4 + PLL_FS2_LATCH*2 + PLL_FS1_LATCH;
     return index;
 }
